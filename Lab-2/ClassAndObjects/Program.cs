f64a88051f0e3ea2f7654f86c17f913324d9d127
chapagainsuman427 <chapagainsuman427@gmail.com>
2020-12-10 22:06:44 +0545

    Lab-2_Uploaded

---
+++ Lab-2/ClassAndObjects/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ClassAndObjects
+{
+    public class Example
+    {
+        public void Result()
+        {
+           Console.WriteLine("Correct Result.");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Example Result = new Example();
+            Result.Result();
+        }
+    }
+}
---
+++ Lab-2/Inheritance/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Inheritance
+{
+
+    public class Student
+    {
+        string name; 
+        int rollNo;
+
+        public void addStudent(string Name, int RollNo)
+        {
+            name = Name;
+            rollNo = RollNo;
+        }
+
+        public void personalDetail()
+        {
+            Console.WriteLine("Hello, this is {0} and my roll number is {1}", name, rollNo);
+        }
+    }
+
+     public class MarkSheet: Student 
+    {
+        public int marksInCDC, marksInNCC, marksInSE;
+        float percentage;
+
+
+        public MarkSheet(int CDC, int NCC, int SE)
+        {
+            marksInCDC = CDC;
+            marksInNCC = NCC;
+            marksInSE = SE;
+
+            percentage = (marksInCDC+marksInNCC+marksInSE)/3;
+        }
+
+        public void Result()
+        {
+            addStudent("Suman",15857);
+            this.personalDetail();
+            Console.WriteLine("I got {0} percentage.", percentage);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MarkSheet no1 = new MarkSheet(75,78,65);
+            no1.Result();
+        }
+    }
+}
