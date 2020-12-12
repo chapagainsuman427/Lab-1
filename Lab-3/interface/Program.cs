5aff9b7244b8097256c5d3a25156d87ab6ad1cf0
chapagainsuman427 <chapagainsuman427@gmail.com>
2020-12-12 19:59:06 +0545

    Lab_Uploaded

---
+++ Lab-3/interface/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Interface
+{
+    // Interface for First Parent Class
+    public interface Interface1
+    {
+         void first();
+    }
+
+    // First Parent Class
+    public class TheRock : Interface1 
+     { 
+        public void first() 
+        { 
+            Console.WriteLine("It doesnot matter what your name is.");
+        } 
+    } 
+
+     // Interface for Second Parent Class
+    public interface Interface2
+    {
+        void second();     
+    }
+
+    // Second Parent Class
+    class StoneCold : Interface2 
+    { 
+        public void second() 
+        { 
+            Console.WriteLine("Showing response.");
+        } 
+    } 
+
+    // Multiple Inheritance through Interface1 and Interface2
+    class ChildClass : Interface1, Interface2
+    { 
+        TheRock obj1 = new TheRock(); 
+        StoneCold obj2 = new StoneCold(); 
+        public void first() 
+        {
+            Console.Write("What is your name? ");
+            Console.ReadLine();
+            obj1.first();
+        } 
+        public void second() 
+        {
+            Console.WriteLine("\nIf you like it, give some response!!!");
+            obj2.second(); 
+        } 
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ChildClass obj = new ChildClass();
+
+            obj.first();
+            obj.second();
+        }
+    }
+
+}
